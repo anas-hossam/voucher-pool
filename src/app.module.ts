5af3dda7284b3d95875ef4d041be3c98973802b0
anas.hossam <softeng.anas@gmail.com>
2023-01-05 11:56:11 +0200

    feat: :sparkles: use modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { CustomerModule } from './customer/customer.module';
+import { VoucherModule } from './voucher/voucher.module';
+import { OfferModule } from './offer/offer.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    CustomerModule,
+    VoucherModule,
+    OfferModule,
   ],
   controllers: [AppController],
   providers: [AppService],
